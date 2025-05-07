38a81fbd6c8291cc5818a09659470c4699bda87c
Gabriel <sars041822@gmail.com>
2025-05-07 11:48:36 -0300

    commit aula 07.05.2025

--- Flutter/app_feirinha/lib/src/auth/sign_in_screen.dart
+++ Flutter/app_feirinha/lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:app_feirinha/src/auth/components/custom_text.dart';
 import 'package:flutter/material.dart';
 
@@ -12,84 +13,132 @@ class SignInScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Colors.green,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              //espaçamento na horizontal e vertical
-              padding: EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 40,
-              ),
-              //arredondamento de bordas
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top:Radius.circular(45)
-                )
-              ),
-              child: Column(
-                children: [
-                  //email
-                  CustomTextField(
-                    icon: Icons.email,
-                    label: 'Email',
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text.rich(
+                  TextSpan(
+                  style: TextStyle(
+                    fontSize: 40,
                   ),
-                  //senha
-                  CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isObscure: true,
-                  ),
-                  //botão entrar
-                  SizedBox(
-                    // altura
-                    height: 50,
-                    //comprimento
-                    width: 600,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        )
+                  children: [ 
+                    TextSpan(
+                      text: 'Feir',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                      onPressed: () {}, 
-                    child: Text('Entrar',
-                    style: TextStyle(fontSize: 18,),),),
+                    ),
+                    TextSpan(
+                      text: 'inha',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                      ],
+                    ),
                   ),
-                  Row(children: [
-                    Expanded(child: Divider(
-                      color: Colors.grey.withAlpha(90),
-                      thickness: 1,
-                    ),),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: Text('Ou'),
-                    ), 
-                    Expanded(child: Divider(
-                      color: Colors.grey.withAlpha(90),
-                      thickness: 1,
-                    ),),    
-                  ],),
-                  SizedBox(
-                    height: 50,
-                    width: 600,
-                    child: OutlinedButton(onPressed: () {},
-                    //formatação do botão
-                    style: OutlinedButton.styleFrom(
+      SizedBox(
+        height: 50,
+        child: DefaultTextStyle(
+        style: const TextStyle(
+          fontSize: 25.0,
+          fontFamily: 'Horizon',
+          color: Colors.white
+        ),
+        child: AnimatedTextKit(
+          repeatForever: true,
+          animatedTexts: [
+            RotateAnimatedText('Frutas'),
+            RotateAnimatedText('Legumes'),
+            RotateAnimatedText('Verduras'),
+            RotateAnimatedText('Temperos'),
+          ],
+          onTap: () {
+            print("Tap Event");
+          },
+        ),
+            ),
+      ),
+    ],
+  ),
+),
+
+
+          Container(
+            //espaçamento na horizontal e vertical
+            padding: EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            //arredondamento de bordas
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top:Radius.circular(45)
+              )
+            ),
+            child: Column(
+              children: [
+                //email
+                CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                //senha
+                CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isObscure: true,
+                ),
+                //botão entrar
+                SizedBox(
+                  // altura
+                  height: 50,
+                  //comprimento
+                  width: 600,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      side: BorderSide(
-                        width: 1,
-                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(18),
                       )
                     ),
-                    child: Text('Criar conta',style: TextStyle(fontSize: 18,),),),
-                    ),
-                ],
-              ),
+                    onPressed: () {}, 
+                  child: Text('Entrar',
+                  style: TextStyle(fontSize: 18,),),),
+                ),
+                Row(children: [
+                  Expanded(child: Divider(
+                    color: Colors.grey.withAlpha(90),
+                    thickness: 1,
+                  ),),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text('Ou'),
+                  ), 
+                  Expanded(child: Divider(
+                    color: Colors.grey.withAlpha(90),
+                    thickness: 1,
+                  ),),    
+                ],),
+                SizedBox(
+                  height: 50,
+                  width: 600,
+                  child: OutlinedButton(onPressed: () {},
+                  //formatação do botão
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    side: BorderSide(
+                      width: 1,
+                      color: Colors.black,
+                    )
+                  ),
+                  child: Text('Criar conta',style: TextStyle(fontSize: 18,),),),
+                  ),
+
+              ],
             ),
           )
         ],
